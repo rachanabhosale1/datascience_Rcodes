a8098931e88ee46d9389461581aebca152cd2655
RACHANA BHOSLE <76518676+rachanabhosale1@users.noreply.github.com>
2021-03-14 14:25:46 +0530

    Add files via upload

---
+++ R pratices.R
@@ -0,0 +1,75 @@
+1+1
+
+2+3*4
+3^2
+
+exp(1)
+sqrt(10)
+
+pi
+2*pi*6378
+exp(2)
+exp(3)
+x<-1
+y<-4
+z<-5
+
+
+
+
+x*y*z
+#how to creat vector
+#creating of vector
+Age<-c(22,24,26,27)
+x<-c(2,0,0,4)
+y<-c(1,9,9,9)
+
+#additin of vector
+x*y
+x+y
+#applying sqrt funvtion on vector
+sqrt(x)
+sqrt(y)
+#scalr mult of vector
+x*5
+#acessing the vector
+x[2]
+y[4]
+x[-1]
+x[2]
+
+
+
+
+
+
+
+#acessing vector element'
+x<-c(2,0,0,4)
+x[1]
+x[-1]
+x[1]<-3;x
+x[-1]=5;x
+y<9
+y[4]=1
+y<9
+y[y<9]=2
+y
+
+
+
+
+#data frame
+df<-data.frame(x=1:3,y=c("a","b","c"))
+df
+Df_new<-data.frame(height=c(150,160),weight=c(65,72))
+Df_new
+df[c(1),c(1,2)]
+df[1,1]
+df[1,2]
+df[c(1,3),c(2)] # 1,3 means in row and 2 means coloum 
+df[c(1,3),1]
+df[c(1,3),c(1,2)] # before ,(coma) row and after ,coma column
+
+
+
